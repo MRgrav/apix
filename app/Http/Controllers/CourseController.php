bad42619028a7bd8d1ab358a03c67a3c71c3cf6d
MRgrav <gauravnetuser@gmail.com>
2025-01-07 19:23:10 +0530

    minor char issue

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -368,8 +368,8 @@ class CourseController extends Controller
             $key = $userId . 'purchase';
 
             // Try to fetch the purchased courses from Redis cache
-            if (Cache::has('classwix_:' . $key)) {
-                $courses = json_decode(Cache::get('classwix_:' . $key), true); // Decode the JSON data
+            if (Cache::has('classwix_' . $key)) {
+                $courses = json_decode(Cache::get('classwix_' . $key), true); // Decode the JSON data
                 return response()->json([
                     'message' => 'Purchased courses retrieved from cache successfully',
                     'courses' => $courses,
