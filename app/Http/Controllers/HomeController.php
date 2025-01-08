3a6eff467ecbeaef2ef2fc44d6509803b90b8534
MRgrav <gauravnetuser@gmail.com>
2025-01-08 17:44:27 +0530

    model import was not there, lol

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\CourseCategory;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
+use App\Models\Group;
 
 class HomeController extends Controller
 {
