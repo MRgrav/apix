9f404fb40931d571623c31f458c72b4e21c286bc
MRgrav <gauravnetuser@gmail.com>
2025-05-10 17:45:32 +0530

    issue fixed

--- database/migrations/2025_05_10_162059_update_thumbnail_datatype.php
+++ database/migrations/2025_05_10_162059_update_thumbnail_datatype.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            $table->string('thumbnail')->change();
+            $table->string('thumbnail')->nullable()->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            //
+            
         });
     }
 };
