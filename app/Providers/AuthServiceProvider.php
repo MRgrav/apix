791ad07a568394e09c33c9e1166e80f205d11b3f
MRgrav <gauravnetuser@gmail.com>
2025-06-12 18:32:01 +0530

    new user log

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('viewLogViewer', function ($user = null) {
             // Replace 'YOUR_IP_ADDRESS' with your actual public IP (e.g., '203.0.113.42')
             \Log::info('LogViewer attempt from IP: ' . request()->ip() . 'cookies: ' . json_encode(request()->cookies->all()));
+            \Log::info('user: ' . $user . ' phone: ' . $user->phone);
             $allowedIps = ['YOUR_PUBLIC_IP', 'YOUR_PUBLIC_IPV6'];
             return in_array(request()->ip(), $allowedIps);
             // && request()->cookie('logviewer_key') === 'your_secret_value';
